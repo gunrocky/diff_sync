0742bf0c2c234f1d2203fb47f3d1d1062dffe1ff
gunrocky <gunrocky@users.noreply.github.com>
2016-03-27 12:42:24 +0300

    Added task description

--- main.cpp
+++ main.cpp
@@ -7,6 +7,15 @@
 /* 
  * File:   main.cpp
  * Author: gunrocky
+ * Task: Exploring the differences and purposes of various synchronization primitives
+ * in practice Write a program that can run N parallel threads (N may change during startup).
+ * Threads only in large loops increment a single shared variable
+ * and perform minimal latency (1 millisecond to simulate busy computing activity).
+ * Provide control over the blocking mode of the thread's working cycle, at a minimum:
+ * a) no blocking at all,
+ * b) locking on a mutex,
+ * c) lock on spinlock.
+ * Analyze the difference in the behavior of the 3 options.
  *
  * Created on March 27, 2016, 12:34 PM
  */
